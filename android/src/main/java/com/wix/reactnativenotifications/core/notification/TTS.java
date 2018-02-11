2b6dcd8c4cdb4636705aa74ed5495fcba88c3d9a
Raymond Penners <raymond.penners@intenct.nl>
2018-02-11 17:15:12 +0100

    fix(tts): Android 4.4 java.lang.NoSuchMethodError

--- android/src/main/java/com/wix/reactnativenotifications/core/notification/TTS.java
+++ android/src/main/java/com/wix/reactnativenotifications/core/notification/TTS.java
@@ -1,9 +1,12 @@
 package com.wix.reactnativenotifications.core.notification;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import java.util.Locale;
+import java.util.HashMap;
 
 class TTS implements TextToSpeech.OnInitListener {
 
@@ -44,8 +47,26 @@ class TTS implements TextToSpeech.OnInitListener {
             } else if (engine.isLanguageAvailable(Locale.ENGLISH) == TextToSpeech.LANG_AVAILABLE) {
                 engine.setLanguage(Locale.ENGLISH);
             }
-            engine.speak(text, TextToSpeech.QUEUE_ADD, null, "tts");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ttsGreater21(text);
+            } else {
+                ttsUnder20(text);
+            }
+
         } else if (status == TextToSpeech.ERROR) {
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "tts");
+        engine.speak(text, TextToSpeech.QUEUE_ADD, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        engine.speak(text, TextToSpeech.QUEUE_ADD, null, "tts");
     }
 }
